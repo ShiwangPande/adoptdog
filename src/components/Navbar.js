fbcf2e6d88c900787f6b123faa77f130359eafbd
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2024-06-09 00:25:17 +0530

    added features in pet tracking

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="bg-black p-4">
             <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-white text-2xl font-bold hover:text-yellow-300 transition duration-300">Pet Adoption</Link>
+                <Link to="/" className="text-white text-2xl font-bold hover:text-yellow-300 transition duration-300">LittlePaw</Link>
                 <div className="flex items-center">
                     <Link to="/" className="text-white mr-6 text-lg hover:text-yellow-300 transition duration-300">Home</Link>
                     <Link to="/trackpage" className="text-white mr-6  text-lg hover:text-yellow-300 transition duration-300">Track</Link>
